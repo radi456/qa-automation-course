336553d50eace8d97042d48c1f5fe5cbc115f4fc
radi456 <radoslava_kostova@mail.bg>
2025-10-05 21:11:19 +0300

    fourth commit

--- lectures/01-develepment-env-basics/homework/task01/verify-setup.js
+++ lectures/01-develepment-env-basics/homework/task01/verify-setup.js
@@ -1,15 +1,20 @@
 function checkNodeVersion() {
-  const currentVersion = process.version;
+  const currentVersion = process.version();
   console.log("Node.js version: " + currentVersion);
 }
 checkNodeVersion();
 
 function checkNpmVersion() {
-  const currentNpmVersion = process.env.npm.version;
+  const currentNpmVersion = process.env.npm.version();
   console.log("Npm version: " + currentNmpVersion);
 }
 checkNpmVersion();
 
-function displayEnvironmentInfo (){
-  
+function displayEnvironmentInfo() {
+  const systemInfo = process.platform();
+  const workingDirectory = process.cwd();
+
+  console.log("System info: " + systemInfo);
+  console.log("Working directory: " + workingDirectory);
 }
+displayEnvironmentInfo();
