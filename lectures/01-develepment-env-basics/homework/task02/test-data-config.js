439f399a827a0bd49c1582ad9eadf35f3721b275
radi456 <radoslava_kostova@mail.bg>
2025-10-05 22:04:54 +0300

    Task2 commit

--- lectures/01-develepment-env-basics/homework/task02/test-data-config.js
+++ lectures/01-develepment-env-basics/homework/task02/test-data-config.js
@@ -1,23 +1,36 @@
-const BASE_URL= "http://demo-qa-site.com";
+const BASE_URL = "http://demo-qa-site.com";
 const DEFAULT_TIMEOUT = 30000;
 const API_ENDPOINT = {
-    login: "http://demo-qa-site.com/api/login",
-    users: "http://demo-qa-site.com/api/users",
-    products: "http://demo-qa-site.com/api/products",
-}
-let testUserEmail= 'example@mail.com';
-let testUserPasword= '74loir!';
-let adminUserEmail= 'admin@mail.com';
-let expectedWelcomeMessage = 'Welcome to your dashboard';
+  login: "http://demo-qa-site.com/api/login",
+  users: "http://demo-qa-site.com/api/users",
+  products: "http://demo-qa-site.com/api/products",
+};
+let testUserEmail = "example@mail.com";
+let testUserPasword = "74loir!";
+let adminUserEmail = "admin@mail.com";
+let expectedWelcomeMessage = "Welcome to your dashboard";
 
 let debugMode = true;
 let runSlowTests = false;
 let useTestData = true;
 
-function validateEmail(mail){
-    console.log("Validation email format:" + testUserEmail.includes ('@'));
-    console.log("Validation email format:" + testUserEmail.includes ('.'));
-    if (testUserEmail.includes ('@') && testUserEmail.includes ('.')){
-        return true;
-    } else {return false;}
-console.log("Test of email validation format:" +validateEmail(testUserEmail));}
+function validateEmail(mail) {
+  console.log("Validation email format:" + testUserEmail.includes("@"));
+  console.log("Validation email format:" + testUserEmail.includes("."));
+  if (testUserEmail.includes("@") && testUserEmail.includes(".")) {
+    return true;
+  } else {
+    return false;
+  }
+  console.log(
+    "Test of email validation format:" + validateEmail(testUserEmail)
+  );
+}
+
+function validatePassword(password) {
+  console.log(
+    "Pasword lenght is less than 8 characters:" + testUserPasword.length < 8
+  );
+  return false;
+  console.log();
+}
