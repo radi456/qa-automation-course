f626b32c8176a080190e2fa747b308e8c60e00bd
radi456 <radoslava_kostova@mail.bg>
2025-10-05 21:37:20 +0300

    Test data config commit

---
+++ lectures/01-develepment-env-basics/homework/task02/test-data-config.js
@@ -0,0 +1,19 @@
+const BASE_URL= "http://demo-qa-site.com";
+const DEFAULT_TIMEOUT = 30000;
+const API_ENDPOINT = {
+    login: "http://demo-qa-site.com/api/login",
+    users: "http://demo-qa-site.com/api/users",
+    products: "http://demo-qa-site.com/api/products",
+}
+let testUserEmail= 'example@mail.com';
+let testUserPasword= '74loir!';
+let adminUserEmail= 'admin@mail.com';
+let expectedWelcomeMessage = 'Welcome to your dashboard';
+
+let debugMode = true;
+let runSlowTests = false;
+let useTestData = true;
+
+function validateEmail(){
+    
+}
